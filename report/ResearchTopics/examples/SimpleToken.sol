e66c1eb09fba6f0cf6b27e2a61643ffe8f5d10ee
Lars Stegeman <lstegeman94@gmail.com>
2018-04-04 15:52:46 +0200

    Add solidity highlighting, fix spacing

--- report/ResearchTopics/examples/SimpleToken.sol
+++ report/ResearchTopics/examples/SimpleToken.sol
@@ -1,24 +1,28 @@
 pragma solidity ^0.4.20;
 
 contract SimpleToken {
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-	/* TotalSupply is fixed for this token, and does not change. It is assigned in the constructor */
+	/* This creates an array with all balances */
+	mapping (address => uint256) public balanceOf;
+	/* TotalSupply is fixed for this token, and does not change. */
+	/* It is assigned in the constructor */
 	uint256 totalSupply;
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function SimpleToken(
-        uint256 initialSupply
-        ) public {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+	/* Initializes contract with initial supply tokens to the creator of the contract */
+	function SimpleToken(uint256 initialSupply) public {
+		// Give the creator all initial tokens
+		balanceOf[msg.sender] = initialSupply;              
 		totalSupply = initialSupply;
-    }
+	}
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) public {
-        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
-        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
-        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
-        balanceOf[_to] += _value;                           // Add the same to the recipient
-    }
+	/* Send coins */
+	function transfer(address _to, uint256 _value) public {
+		// Check if the sender has enough
+		require(balanceOf[msg.sender] >= _value);
+		// Check for overflows
+		require(balanceOf[_to] + _value >= balanceOf[_to]); 
+		// Subtract from the sender
+		balanceOf[msg.sender] -= _value;    
+		// Add the same to the recipient		
+		balanceOf[_to] += _value;                           
+	}
 }
--- report/ResearchTopics/examples/SimpleToken.spec.sol
+++ report/ResearchTopics/examples/SimpleToken.spec.sol
@@ -1,21 +1,23 @@
 pragma solidity ^0.4.20;
 
 contract SimpleToken {
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-	/* TotalSupply is fixed for this token, and does not change. It is assigned in the constructor */
+	/* This creates an array with all balances */
+	mapping (address => uint256) public balanceOf;
+	/* TotalSupply is fixed for this token, and does not change. */
+	/* It is assigned in the constructor */
 	uint256 totalSupply;
-	
-	
+
+
 	/* @invariant totalSupply ==  sum = (forall x in balanceOf [ sum +=balanceOf[x]])
 
-    /* Send coins */
-	/* @ensures 	balanceOf[_to] == \old(balanceOf[_to]) + _value &&
-					balanceOf[msg.sender] == \old(balanceOf[msg.sender]) - _value &&
-					forall x : x != _to || x != msg.sender : balanceOf[x] == \old(balanceOf[x])
-					||	
-					forall x: balanceOf[x] == \old(balanceOf[x])
+	/* Send coins */
+	/* @ensures 	
+			balanceOf[_to] == \old(balanceOf[_to]) + _value &&
+			balanceOf[msg.sender] == \old(balanceOf[msg.sender]) - _value &&
+			forall x : x != _to || x != msg.sender : balanceOf[x] == \old(balanceOf[x])
+			||	
+			forall x: balanceOf[x] == \old(balanceOf[x])
 	*/
-    function transfer(address _to, uint256 _value) public {
-    }
+	function transfer(address _to, uint256 _value) public {
+	}
 }
